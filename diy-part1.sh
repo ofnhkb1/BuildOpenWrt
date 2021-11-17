25ffd6ef9c3b51bd1738adc383c74a5235afc027
chenmo <ofnhkb1@126.com>
2021-11-17 11:10:02 +0800

    修改一些默认配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,11 +19,15 @@ git clone --depth=1 https://github.com/kenzok8/small
 
 
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 git clone --depth=1 https://github.com/rosywrt/luci-theme-rosy.git
 git clone --depth=1 https://github.com/sypopo/luci-theme-argon-mc.git
 git clone --depth=1 https://github.com/apollo-ng/luci-theme-darkmatter.git
 
 cd -
 sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+#修改默认网关
 sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
+#修改主机名
+sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
+#修改默认ssid
+sed -i 's/OpenWrt/Chenmo/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
