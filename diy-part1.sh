723070c863b391c97db29ac229efc774935a1915
chenmo <ofnhkb1@126.com>
2021-11-17 11:16:42 +0800

    fix

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,3 +31,4 @@ sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
 #修改默认ssid
 sed -i 's/OpenWrt/Chenmo/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#
